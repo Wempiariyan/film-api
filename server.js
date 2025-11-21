90ece412834f70ab83543dae6d6fc5a6aaf55adf
Wempi Ariyan <wempiariyan0@.com>
2025-11-21 16:17:14 +0700

    Initial commit:Setup Express project structur

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/status', (req, res) => {
 });
 
 // === AUTH ROUTES (Refactored for pg) ===
-app.post('/auth/register', async (req, res, next) => {
+app.post('/authMiddleware/register', async (req, res, next) => {
   const { username, password } = req.body;
 
   if (!username || !password || password.length < 6) {
